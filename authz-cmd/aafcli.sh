53bd2f3a14c1591c444b7dbe99f1cde611ae861b
sg481n <sg481n@att.com>
2017-09-18 16:35:50 -0400

    Update AAF project version to release docker image
    Updated project version to 1.0.1-SNAPSHOT and modified few
    aaf property files to release docker image of aaf to nexts onap.
    
    Change-Id: Ic048da22d69b8030974b97bb118892d4b9328f21
    Issue-id: AAF-60
    Signed-off-by: sg481n <sg481n@att.com>

--- authz-cmd/aafcli.sh
+++ authz-cmd/aafcli.sh
@@ -8,6 +8,6 @@ DME2REG=/opt/dme2reg
 	#-DDME2_EP_REGISTRY_CLASS=DME2FS -DAFT_DME2_EP_REGISTRY_FS_DIR=$DME2REG \
 	#com.att.cmd.AAFcli $*
 
-CLASSPATH=/opt/app/aaf/authz-service/1.0.0-SNAPSHOT/etc:/opt/app/aaf/authz-service/1.0.0-SNAPSHOT/lib/authz-cmd-1.0.0-SNAPSHOT-jar-with-dependencies.jar  
+CLASSPATH=/opt/app/aaf/authz-service/etc:/opt/app/aaf/authz-service/lib/authz-cmd-1.0.1-SNAPSHOT-jar-with-dependencies.jar  
 #java -cp $CLASSPATH -Dcadi_prop_files=../authz-service/src/main/sample/authAPI.props -DDME2_EP_REGISTRY_CLASS=DME2FS -DAFT_DME2_EP_REGISTRY_FS_DIR=$DME2REG com.att.cmd.AAFcli $*
-java -cp $CLASSPATH -Dcadi_prop_files=/opt/app/aaf/authz-service/1.0.0-SNAPSHOT/etc/authAPI.props -DDME2_EP_REGISTRY_CLASS=DME2FS -DAFT_DME2_EP_REGISTRY_FS_DIR=$DME2REG org.onap.aaf.cmd.AAFcli $*
+java -cp $CLASSPATH -Dcadi_prop_files=/opt/app/aaf/authz-service/etc/authAPI.props -DDME2_EP_REGISTRY_CLASS=DME2FS -DAFT_DME2_EP_REGISTRY_FS_DIR=$DME2REG org.onap.aaf.cmd.AAFcli $*
