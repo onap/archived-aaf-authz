898a279fcde65f7889d3390501180b6ff7bb95d9
sg481n <sg481n@att.com>
2017-09-19 11:39:32 -0400

    Need to release authz-client alone for AAF
    removing other submodules of aaf as authz-client snapshot version needs
    to reelase earlier than any other sub modules.I am going to commit other submodules
    in the next commit.
    
    Issue-id: AAF-64
    Change-Id: I062c2bb879b5a09a285cd3c1f13419669d8470c7
    Signed-off-by: sg481n <sg481n@att.com>

--- authz-batch/src/main/java/com/att/authz/actions/Key.java
+++
@@ -1,8 +0,0 @@
-/*******************************************************************************
- * Copyright (c) 2016 AT&T Intellectual Property. All rights reserved.
- *******************************************************************************/
-package com.att.authz.actions;
-
-public interface Key<HELPER> {
-	public String key(HELPER H);
-}
