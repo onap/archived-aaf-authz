a139c7a08aeb1f5d85ba4b6d93d735f68edd0cd1
IanHowell <ian.howell@att.com>
2018-04-02 09:02:42 -0500

    Get test coverage of UserChainManip to 100%
    Issue-ID: AAF-128
    Change-Id: I41d6a2c3f405d8959507ae02c934fffff7903e32
    Signed-off-by: IanHowell <ian.howell@att.com>

--- cadi/core/src/test/java/org/onap/aaf/cadi/test/util/JU_UserChainManip.java
+++ cadi/core/src/test/java/org/onap/aaf/cadi/test/util/JU_UserChainManip.java
@@ -6,9 +6,9 @@
  * * Licensed under the Apache License, Version 2.0 (the "License");
  * * you may not use this file except in compliance with the License.
  * * You may obtain a copy of the License at
- * * 
+ * *
  *  *      http://www.apache.org/licenses/LICENSE-2.0
- * * 
+ * *
  *  * Unless required by applicable law or agreed to in writing, software
  * * distributed under the License is distributed on an "AS IS" BASIS,
  * * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -20,12 +20,10 @@
  ******************************************************************************/
 package org.onap.aaf.cadi.test.util;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.junit.*;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.junit.Test;
 import org.onap.aaf.cadi.UserChain;
 import org.onap.aaf.cadi.util.UserChainManip;
 
@@ -34,33 +32,36 @@ public class JU_UserChainManip {
 	@Test
 	public void build(){
 		UserChain.Protocol baseAuth=UserChain.Protocol.BasicAuth;
-		assertEquals(UserChainManip.build(new StringBuilder(""), "app", "id", baseAuth, true).toString(), "app:id:BasicAuth:AS");
-	}
-	
+		StringBuilder sb = UserChainManip.build(new StringBuilder(""), "app", "id", baseAuth, true);
+		assertThat(sb.toString(), is("app:id:BasicAuth:AS"));
 
-	@Test
-	public void idToNS(){
-		assertEquals(UserChainManip.idToNS(null), "");
-	}
-	
-	@Test
-	public void idToNS1(){
-		assertEquals(UserChainManip.idToNS("t@st"), "st");
+		// for coverage
+		sb = UserChainManip.build(sb, "app", "id", baseAuth, true);
+		assertThat(sb.toString(), is("app:id:BasicAuth:AS,app:id:BasicAuth"));
+
+		sb = UserChainManip.build(new StringBuilder(""), "app", "id", baseAuth, false);
+		assertThat(sb.toString(), is("app:id:BasicAuth"));
 	}
 
 	@Test
-	public void test() {
-		assertEquals("",UserChainManip.idToNS(null));
-		assertEquals("",UserChainManip.idToNS(""));
-		assertEquals("",UserChainManip.idToNS("something"));
-		assertEquals("",UserChainManip.idToNS("something@@"));
-		assertEquals("",UserChainManip.idToNS("something@@."));
-		assertEquals("com",UserChainManip.idToNS("something@com"));
-		assertEquals("com.random",UserChainManip.idToNS("something@random.com"));
-		assertEquals("com.random",UserChainManip.idToNS("@random.com"));
-		assertEquals("com.random",UserChainManip.idToNS("something@random.com."));
-		assertEquals("com.random",UserChainManip.idToNS("something@..random...com..."));
-		assertEquals("com.random.this",UserChainManip.idToNS("something@this.random.com"));
+	public void idToNSTEST() {
+		assertThat(UserChainManip.idToNS(null), is(""));
+		assertThat(UserChainManip.idToNS(""), is(""));
+		assertThat(UserChainManip.idToNS("something"), is(""));
+		assertThat(UserChainManip.idToNS("something@@"), is(""));
+		assertThat(UserChainManip.idToNS("something@@."), is(""));
+		assertThat(UserChainManip.idToNS("something@com"), is("com"));
+		assertThat(UserChainManip.idToNS("something@random.com"), is("com.random"));
+		assertThat(UserChainManip.idToNS("@random.com"), is("com.random"));
+		assertThat(UserChainManip.idToNS("something@random.com."), is("com.random"));
+		assertThat(UserChainManip.idToNS("something@..random...com..."), is("com.random"));
+		assertThat(UserChainManip.idToNS("something@this.random.com"), is("com.random.this"));
+	}
+
+	@Test
+	public void coverageTest() {
+		@SuppressWarnings("unused")
+		UserChainManip ucm = new UserChainManip();
 	}
 
 }
