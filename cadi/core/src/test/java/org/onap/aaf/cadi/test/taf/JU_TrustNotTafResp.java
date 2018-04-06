983970a57ac9c345eaf8005b07460714d82787a6
IanHowell <ian.howell@att.com>
2018-04-06 09:57:16 -0500

    Improve JUnit test coverage
    * Fixed a typo in DenialOfServiceTaf
    * Added a JUnit for DenialOfServiceTaf
    * Cleaned up JU_Cached, JU_AuthzEnv (WIP)
    
    Issue-ID: AAF-128
    Change-Id: I76ace8aff3f72538f40068f6cc9c80f24aee7403
    Signed-off-by: IanHowell <ian.howell@att.com>
    
    Increase coverage in cadi/client and cadi/core
    
    Get coverage for Holder, NulTaf, and NullTafResp to 100%
    
    Issue-ID: AAF-128
    Change-Id: Ica83c29311801d499f922ccb4c33fb7f4e58f686
    Signed-off-by: IanHowell <ian.howell@att.com>
    
    Improve coverage of the Tafs
    
    Issue-ID: AAF-128
    Change-Id: I07f9ebee0a64c598047e74801a266898107783f7
    Signed-off-by: IanHowell <ian.howell@att.com>

---
+++ cadi/core/src/test/java/org/onap/aaf/cadi/test/taf/JU_TrustNotTafResp.java
@@ -0,0 +1,72 @@
+/*******************************************************************************
+ * ============LICENSE_START====================================================
+ * * org.onap.aaf
+ * * ===========================================================================
+ * * Copyright © 2017 AT&T Intellectual Property. All rights reserved.
+ * * ===========================================================================
+ * * Licensed under the Apache License, Version 2.0 (the "License");
+ * * you may not use this file except in compliance with the License.
+ * * You may obtain a copy of the License at
+ * * 
+ *  *      http://www.apache.org/licenses/LICENSE-2.0
+ * * 
+ *  * Unless required by applicable law or agreed to in writing, software
+ * * distributed under the License is distributed on an "AS IS" BASIS,
+ * * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * * See the License for the specific language governing permissions and
+ * * limitations under the License.
+ * * ============LICENSE_END====================================================
+ * *
+ * *
+ ******************************************************************************/
+package org.onap.aaf.cadi.test.taf;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import static org.mockito.Mockito.*;
+import org.junit.*;
+import org.mockito.*;
+
+import java.io.IOException;
+
+import org.onap.aaf.cadi.Access;
+import org.onap.aaf.cadi.taf.TafResp;
+import org.onap.aaf.cadi.taf.TafResp.RESP;
+import org.onap.aaf.cadi.taf.TrustNotTafResp;
+import org.onap.aaf.cadi.principal.TaggedPrincipal;
+
+public class JU_TrustNotTafResp {
+
+	@Mock
+	TafResp delegateMock;
+
+	@Mock
+	TaggedPrincipal principalMock;
+
+	@Mock
+	Access accessMock;
+
+	private final String description = "Example Description";
+
+	@Before
+	public void setup() throws IOException {
+		MockitoAnnotations.initMocks(this);
+
+		when(delegateMock.getPrincipal()).thenReturn(principalMock);
+		when(delegateMock.getAccess()).thenReturn(accessMock);
+	}
+
+	@Test
+	public void test() throws IOException {
+		TrustNotTafResp ttr = new TrustNotTafResp(delegateMock, description);
+		assertThat(ttr.isValid(), is(false));
+		assertThat(ttr.desc(), is(description));
+		assertThat(ttr.authenticate(), is(RESP.NO_FURTHER_PROCESSING));
+		assertThat(ttr.isAuthenticated(), is(RESP.NO_FURTHER_PROCESSING));
+		assertThat(ttr.getPrincipal(), is(principalMock));
+		assertThat(ttr.getAccess(), is(accessMock));
+		assertThat(ttr.isFailedAttempt(), is(true));
+		assertThat(ttr.toString(), is(description));
+	}
+
+}
