4d7da3d5edf32a424d786b83fbdb89a1f57451ac
Instrumental <jcgmisc@stl.gathman.org>
2018-04-06 08:22:42 -0500

    Fix Locate Cassandra config issue
    Issue-ID: AAF-216
    Change-Id: I52719ee951a5135be5561fca6ab71744a014fe41
    Signed-off-by: Instrumental <jcgmisc@stl.gathman.org>

---
+++ auth/docker/dstart.sh
@@ -0,0 +1,12 @@
+#!/bin/bash dstop.sh
+. d.props
+
+if [ "$1" == "" ]; then
+  AAF_COMPONENTS=`ls -r ../aaf_${VERSION}/bin | grep -v '\.'`
+else
+  AAF_COMPONENTS=$1
+fi
+
+for AAF_COMPONENT in ${AAF_COMPONENTS}; do
+  docker start aaf_$AAF_COMPONENT
+done
