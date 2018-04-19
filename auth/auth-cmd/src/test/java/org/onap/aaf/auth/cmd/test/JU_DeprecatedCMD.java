f5360aaa76c339e731bdbb6dd65933489c946fad
gabe.maurer <gabe.maurer@att.com>
2018-04-19 15:20:37 -0500

    Fixed offnetwork issue
    Issue-ID: AAF-237
    Change-Id: I8a4ddc7535f2b9b077c7b721316d6ee15b29dca6
    Signed-off-by: gabe.maurer <gabe.maurer@att.com>

---
+++ auth/auth-cmd/src/test/java/org/onap/aaf/auth/cmd/test/JU_DeprecatedCMD.java
@@ -0,0 +1,78 @@
+/*******************************************************************************
+ * ============LICENSE_START====================================================
+ * * org.onap.aaf
+ * * ===========================================================================
+ * * Copyright © 2017 AT&T Intellectual Property. All rights reserved.
+ * * ===========================================================================
+ * * Licensed under the Apache License, Version 2.0 (the "License");
+ * * you may not use this file except in compliance with the License.
+ * * You may obtain a copy of the License at
+ * * 
+ *  *      http://www.apache.org/licenses/LICENSE-2.0
+ * * 
+ *  * Unless required by applicable law or agreed to in writing, software
+ * * distributed under the License is distributed on an "AS IS" BASIS,
+ * * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * * See the License for the specific language governing permissions and
+ * * limitations under the License.
+ * * ============LICENSE_END====================================================
+ * *
+ * *
+ ******************************************************************************/
+
+package org.onap.aaf.auth.cmd.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.onap.aaf.auth.cmd.AAFcli;
+import org.onap.aaf.auth.cmd.Cmd;
+import org.onap.aaf.auth.cmd.DeprecatedCMD;
+import org.onap.aaf.auth.cmd.Param;
+import org.onap.aaf.auth.cmd.test.JU_Cmd;
+import org.onap.aaf.cadi.CadiException;
+import org.onap.aaf.cadi.LocatorException;
+import org.onap.aaf.misc.env.APIException;
+
+import static org.mockito.Mockito.*;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+
+import org.junit.Test;
+
+public class JU_DeprecatedCMD {
+
+	CmdStub cmd;
+	AAFcli cli;
+	
+	private class CmdStub extends Cmd {
+
+		public CmdStub(AAFcli aafcli, String name, Param[] params) {
+			super(aafcli, name, params);
+			// TODO Auto-generated constructor stub
+		}
+
+		@Override
+		protected int _exec(int idx, String... args) throws CadiException, APIException, LocatorException {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+		
+	}
+	
+	@Test
+	public void testExec() throws CadiException, APIException, LocatorException, GeneralSecurityException, IOException {
+		cli = JU_AAFCli.getAAfCli();
+		Param[] param = new Param[] {new Param("name",true)};
+		
+		cmd = new CmdStub(cli,"test", param);
+		DeprecatedCMD deprecatedcmd = new DeprecatedCMD(cmd,"test", "test");
+		deprecatedcmd._exec(0, "test");
+	}
+
+}
--- auth/auth-cmd/src/test/java/org/onap/aaf/auth/cmd/test/ns/JU_Create.java
+++ auth/auth-cmd/src/test/java/org/onap/aaf/auth/cmd/test/ns/JU_Create.java
@@ -22,8 +22,17 @@
 package org.onap.aaf.auth.cmd.test.ns;
 
 import org.junit.Assert;
+import org.junit.Before;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.io.Writer;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -33,29 +42,57 @@ import org.onap.aaf.auth.cmd.AAFcli;
 import org.onap.aaf.auth.cmd.ns.Create;
 import org.onap.aaf.auth.cmd.ns.NS;
 import org.onap.aaf.auth.cmd.test.JU_AAFCli;
+import org.onap.aaf.auth.env.AuthzEnv;
+import org.onap.aaf.cadi.CadiException;
+import org.onap.aaf.cadi.Locator;
+import org.onap.aaf.cadi.LocatorException;
+import org.onap.aaf.cadi.PropAccess;
+import org.onap.aaf.cadi.SecuritySetter;
+import org.onap.aaf.cadi.Locator.Item;
+import org.onap.aaf.cadi.http.HMangr;
+import org.onap.aaf.cadi.http.HRcli;
+import org.onap.aaf.misc.env.APIException;
 
 @RunWith(MockitoJUnitRunner.class)
 public class JU_Create {
 
 	private static Create create;//might need to replace import with org.onap.aaf.auth.cmd.perm
-
-	@BeforeClass
-	public static void setUp() throws NoSuchFieldException, SecurityException, Exception, IllegalAccessException {
-		AAFcli cli = JU_AAFCli.getAAfCli();
-		NS ns = new NS(cli);
+	PropAccess prop;
+	AuthzEnv aEnv;
+	Writer wtr;
+	Locator<URI> loc;
+	HMangr hman;	
+	AAFcli aafcli;
+	
+	@Before
+	public void setUp() throws NoSuchFieldException, SecurityException, Exception, IllegalAccessException {
+		prop = new PropAccess();
+		aEnv = new AuthzEnv();
+		wtr = mock(Writer.class);
+		loc = mock(Locator.class);
+		SecuritySetter<HttpURLConnection> secSet = mock(SecuritySetter.class);
+		hman = new HMangr(aEnv, loc);	
+		aafcli = new AAFcli(prop, aEnv, wtr, hman, null, secSet);
+		NS ns = new NS(aafcli);
 		create = new Create(ns);
 	}
 	
 	
 
-//	@Test
-//	public void exec() {
-//		try {
-//			assertEquals(create._exec(0, "add", "del", "reset", "extend"), 500);
-//		} catch (Exception e) {
-//			assertEquals(e.getMessage(), "java.net.UnknownHostException: DME2RESOLVE");
-//		}
-//	}
+	@Test
+	public void testExec() throws APIException, LocatorException, CadiException, URISyntaxException {
+		Item value = mock(Item.class);
+		Locator.Item item = new Locator.Item() {
+		};
+		when(loc.best()).thenReturn(value);
+		URI uri = new URI("http://java.sun.com/j2se/1.3/");
+		when(loc.get(value)).thenReturn(uri);
+		SecuritySetter<HttpURLConnection> secSet = mock(SecuritySetter.class);
+		HRcli hcli = new HRcli(hman, uri, item, secSet);
+		String[] strArr = {"add","upd","del","add","upd","del"};
+		create._exec(0, strArr);
+		
+	}
 
 	@Test
 	public void detailedHelp() {
