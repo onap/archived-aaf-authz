aab3128569c867cec40afd6d7d1dba69026d94e4
Instrumental <jonathan.gathman@att.com>
2018-05-11 17:14:48 -0500

    Cass, Dir
    Issue-ID: AAF-282
    Change-Id: I6a3ad9c5b4dfcc35ab6c6919cda6df18128e8573
    Signed-off-by: Instrumental <jonathan.gathman@att.com>

--- auth/auth-cass/docker/dinstall.sh
+++ auth/auth-cass/docker/dinstall.sh
@@ -1,35 +1,32 @@
 #!/bin/bash 
+DOCKER=/usr/bin/docker
 
-if [ "`docker ps -a | grep aaf_cass`" == "" ]; then
-  docker run --name aaf_cass  -d cassandra:3.11
+if [ "`$DOCKER ps -a | grep aaf_cass`" == "" ]; then
+  $DOCKER run --name aaf_cass  -d cassandra:3.11
   echo "aaf_cass Starting"
-  echo "Check for running Docker Container aaf_cass, then run again."
-  # we have to exit here so that the calling script can load CQL files
-  exit
-else
   for CNT in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15; do
-     if [ "`docker container logs aaf_cass | grep 'listening for CQL clients'`" == "" ]; then
+     if [ "`$DOCKER container logs aaf_cass | grep 'listening for CQL clients'`" == "" ]; then
 	 	echo "Sleep 10"
         sleep 10
      else 
-	 	YESCQL="yes"
      	break
      fi
   done
+  echo "Check for running Docker Container aaf_cass, then run again."
+  exit
 fi 
 
-if [ "$YESCQL" == "" ]; then
-  echo "CQL Never started... exiting"
-  exit
-fi
+sleep 20
+echo "Running containers"
+$DOCKER container ps
 
 echo "Creating /opt/app/cass_init dir on aaf_cass"
-docker exec aaf_cass mkdir -p /opt/app/cass_init
+$DOCKER exec aaf_cass mkdir -p /opt/app/cass_init
 echo "cp the following files to /opt/app/cass_init dir on aaf_cass"
 ls ../src/main/cql
-docker cp "../src/main/cql/." aaf_cass:/opt/app/cass_init
+$DOCKER cp "../src/main/cql/." aaf_cass:/opt/app/cass_init
 echo "The following files are on /opt/app/cass_init dir on aaf_cass"
-docker exec -it aaf_cass ls /opt/app/cass_init
+$DOCKER exec -it aaf_cass ls /opt/app/cass_init
 
 echo "Docker Installed Basic Cassandra on aaf_cass.  Executing the following "
 echo "NOTE: This creator provided is only a Single Instance. For more complex Cassandra, create independently"
@@ -41,10 +38,8 @@ echo " cqlsh -u root -p root -f osaaf.cql"
 echo ""
 echo "The following will give you a temporary identity with which to start working, or emergency"
 echo " cqlsh -u root -p root -f temp_identity.cql"
-echo "Sleeping for 20 seconds"
-sleep 20
 echo "Create Keyspaces and Tables"
-docker exec -it aaf_cass bash -c '\
+$DOCKER exec -it aaf_cass bash -c '\
 cd /opt/app/cass_init; \
 echo "Creating Keyspace";cqlsh -u root -p root -f keyspace.cql;\
 echo "Creating init";cqlsh -u root -p root -f init.cql;\
@@ -52,4 +47,4 @@ echo "Creating osaaf";cqlsh -u root -p root -f osaaf.cql;\
 echo "Creating temp Identity";cqlsh -u root -p root -f temp_identity.cql'
 
 echo "Inspecting aafcassadra.  Use to get the IP address to update org.osaaf.cassandra.props"
-docker inspect aaf_cass | grep '"IPAddress' | head -1
+$DOCKER inspect aaf_cass | grep '"IPAddress' | head -1
