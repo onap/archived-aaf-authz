306e49d4f3a285376e5173fdc9afe94467e7a02c
Instrumental <jonathan.gathman@att.com>
2018-05-18 04:20:08 -0500

    fix truststore builder script
    Issue-ID: AAF-290
    Change-Id: Icffe60736c8f40617477c068c29c14c5ac6b07ea
    Signed-off-by: Instrumental <jonathan.gathman@att.com>

--- conf/CA/truststore.sh
+++ conf/CA/truststore.sh
@@ -1,2 +1,2 @@
 echo "FYI, by convention, truststore passwords are 'changeit', but you may add something more sophisticated"
-openssl pkcs12 -export -name AAF_Root_CA -in certs/ca.crt -inkey private/ca.key -out truststore.p12
+openssl pkcs12 -export -name AAF_Root_CA -in certs/ca.crt -nokeys -out truststore.p12
