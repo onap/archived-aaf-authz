3926bb29e654d5248690735596de26531e3d711e
Instrumental <jonathan.gathman@att.com>
2018-05-19 08:15:14 -0500

    Reduce Cass Mem
    Issue-ID: AAF-273
    Change-Id: Iff3e8eb172328ccbb751d50823542a807c6f3e76
    Signed-off-by: Instrumental <jonathan.gathman@att.com>

--- auth/auth-cass/docker/dinstall.sh
+++ auth/auth-cass/docker/dinstall.sh
@@ -1,13 +1,23 @@
 #!/bin/bash 
-DOCKER=/usr/bin/docker
+if [ -e /usr/bin/docker ]; then 
+  DOCKER=/usr/bin/docker
+elif [ -e /usr/local/bin/docker ]; then
+  DOCKER=/usr/local/bin/docker
+else
+  echo Docker not available in /usr/bin or /usr/local/bin
+  exit
+fi
 echo "Running DInstall"
 if [ "`$DOCKER ps -a | grep aaf_cass`" == "" ]; then
   echo "starting Cass from 'run'"
-  $DOCKER run --name aaf_cass  -d cassandra:3.11
+  # NOTE: These HEAP Sizes are minimal. Not set for full organizations.
+  $DOCKER run --name aaf_cass -e HEAP_NEWSIZE=512M -e MAX_HEAP_SIZE=1024M -e CASSANDRA_DC=dc1 -e CASSANDRA_CLUSTER_NAME=osaaf -d cassandra:3.11 
+  # Set on local Disk
+  # -v /opt/app/cass:/var/lib/cassandra 
   echo "aaf_cass Starting"
   for CNT in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15; do
      if [ "`$DOCKER container logs aaf_cass | grep 'listening for CQL clients'`" == "" ]; then
-	 	echo "Sleep 10"
+	echo "Sleep 10"
         sleep 10
      else 
      	break
@@ -25,22 +35,24 @@ if [ "`$DOCKER ps -a | grep aaf_cass`" == "" ]; then
   echo "The following files are on /opt/app/cass_init dir on aaf_cass"
   $DOCKER exec aaf_cass ls /opt/app/cass_init
   
-  echo "Docker Installed Basic Cassandra on aaf_cass.  Executing the following "
-  echo "NOTE: This creator provided is only a Single Instance. For more complex Cassandra, create independently"
-  echo ""
-  echo " cd /opt/app/cass_init"  
-  echo " cqlsh -f keyspace.cql"
-  echo " cqlsh -f init.cql"
-  echo " cqlsh -f osaaf.cql"
-  echo ""
-  echo "The following will give you a temporary identity with which to start working, or emergency"
-  echo " cqlsh -f temp_identity.cql"
-  echo "Create Keyspaces and Tables"
-  $DOCKER exec aaf_cass bash /usr/bin/cqlsh -f /opt/app/cass_init/keyspace.cql
-  $DOCKER exec aaf_cass bash /usr/bin/cqlsh -e 'describe keyspaces'
-  $DOCKER exec aaf_cass bash /usr/bin/cqlsh -f /opt/app/cass_init/init.cql
-  $DOCKER exec aaf_cass bash /usr/bin/cqlsh -f /opt/app/cass_init/osaaf.cql
-  $DOCKER exec aaf_cass bash /usr/bin/cqlsh -f /opt/app/cass_init/temp_indentity.cql
+  if [ "`$DOCKER exec aaf_cass /usr/bin/cqlsh -e 'describe keyspaces' | grep authz`" == "" ]; then
+    echo "Docker Installed Basic Cassandra on aaf_cass.  Executing the following "
+    echo "NOTE: This creator provided is only a Single Instance. For more complex Cassandra, create independently"
+    echo ""
+    echo " cd /opt/app/cass_init"  
+    echo " cqlsh -f keyspace.cql"
+    echo " cqlsh -f init.cql"
+    echo " cqlsh -f osaaf.cql"
+    echo ""
+    echo "The following will give you a temporary identity with which to start working, or emergency"
+    echo " cqlsh -f temp_identity.cql"
+    echo "Create Keyspaces and Tables"
+    $DOCKER exec aaf_cass bash /usr/bin/cqlsh -f /opt/app/cass_init/keyspace.cql
+    $DOCKER exec aaf_cass bash /usr/bin/cqlsh -e 'describe keyspaces'
+    $DOCKER exec aaf_cass bash /usr/bin/cqlsh -f /opt/app/cass_init/init.cql
+    $DOCKER exec aaf_cass bash /usr/bin/cqlsh -f /opt/app/cass_init/osaaf.cql
+    $DOCKER exec aaf_cass bash /usr/bin/cqlsh -f /opt/app/cass_init/temp_identity.cql
+  fi
 else 
   $DOCKER start aaf_cass
 fi
