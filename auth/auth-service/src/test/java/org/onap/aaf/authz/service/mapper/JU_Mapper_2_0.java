c4906f8883ede123928b30e3174a51c89399b243
Tomek Kaminski <tomasz.kaminski@nokia.com>
2018-07-18 14:25:02 +0200

    Junits for Mapper
    -tests for namespace mappig method
    -little refactor for Question and Define for mocking purpose
    
    Change-Id: I49fb8f8a123c7896e1971913e9f99ef0c049250f
    Issue-ID: AAF-400
    Signed-off-by: Tomek Kaminski <tomasz.kaminski@nokia.com>

--- auth/auth-service/src/test/java/org/onap/aaf/authz/service/mapper/JU_Mapper_2_0.java
+++ auth/auth-service/src/test/java/org/onap/aaf/authz/service/mapper/JU_Mapper_2_0.java
@@ -21,11 +21,141 @@
  ******************************************************************************/
 package org.onap.aaf.authz.service.mapper;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+import static org.mockito.Mockito.verify;
 
+import aaf.v2_0.NsRequest;
+import aaf.v2_0.Request;
+import com.google.common.collect.Lists;
+import java.io.IOException;
+import java.util.List;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.onap.aaf.auth.dao.cass.Namespace;
+import org.onap.aaf.auth.dao.cass.NsType;
+import org.onap.aaf.auth.dao.hl.Question;
+import org.onap.aaf.auth.env.AuthzTrans;
+import org.onap.aaf.auth.layer.Result;
+import org.onap.aaf.auth.service.mapper.Mapper_2_0;
+import org.onap.aaf.cadi.CadiException;
+import org.onap.aaf.misc.env.APIException;
+import org.onap.aaf.misc.env.Env;
 
+@RunWith(MockitoJUnitRunner.class)
 public class JU_Mapper_2_0 {
+
+	private Mapper_2_0 mapper;
+	@Mock
+	private Question question;
+	@Mock
+	AuthzTrans transaction;
+
+	@Before
+	public void setUp() throws APIException, IOException, CadiException {
+		this.mapper = new Mapper_2_0(question);
+	}
+
+	@Test(expected = ClassCastException.class)
+	public void ns_willThrowException_whenInvalidRequestType() {
+		//given
+		Request rq = new Request();
+
+		//when
+		mapper.ns(transaction, rq);
+
+		//then
+		fail("Expected ClassCastException");
+	}
+
+	@Test
+	public void ns_shouldConvertNamespaceRequest_whenValidTypeIsExplicitlyProvided() {
+		//given
+		String namespaceName = "org.companyA.app1";
+		String namespaceType = "APP";
+		NsType expectedNsType = NsType.APP;
+		NsRequest nsRequest = createNsRequestForType(namespaceName, namespaceType);
+
+		//when
+		Result<Namespace> result = mapper.ns(transaction,nsRequest);
+
+		//then
+		assertTrue(result.isOK());
+		assertNamespaceValues(result.value, expectedNsType, namespaceName);
+		verify(transaction).checkpoint(namespaceName,Env.ALWAYS);
+	}
+
+	@Test
+	public void ns_shouldConvertNamespaceRequest_whenInValidTypeIsExplicitlyProvided() {
+		//given
+		String namespaceName = "org.companyA.app1.service0";
+		String invalidNsType = "BLUE";
+		NsType expectedNsType = NsType.APP;
+		NsRequest nsRequest = createNsRequestForType(namespaceName, invalidNsType);
+
+		//when
+		Result<Namespace> result = mapper.ns(transaction,nsRequest);
+
+		//then
+		assertTrue(result.isOK());
+		assertNamespaceValues(result.value, expectedNsType, namespaceName);
+		verify(transaction).checkpoint(namespaceName,Env.ALWAYS);
+	}
+
+	@Test
+	public void ns_shouldConvertRootNamespaceRequest_whenTypeNotProvided() {
+		//given
+		String rootNsName = "org";
+		NsType expectedNsType = NsType.ROOT;
+		NsRequest nsRequest = createNsRequestForType(rootNsName, null);
+
+		//when
+		Result<Namespace> result = mapper.ns(transaction,nsRequest);
+
+		//then
+		assertTrue(result.isOK());
+		assertNamespaceValues(result.value, expectedNsType, rootNsName);
+		verify(transaction).checkpoint(rootNsName,Env.ALWAYS);
+	}
+
+	@Test
+	public void ns_shouldConvertCompanyNamespaceRequest_whenTypeNotProvided() {
+		//given
+		String companyNsName = "org.companyA";
+		NsType expectedNsType = NsType.COMPANY;
+		NsRequest nsRequest = createNsRequestForType(companyNsName, null);
+
+		//when
+		Result<Namespace> result = mapper.ns(transaction,nsRequest);
+
+		//then
+		assertTrue(result.isOK());
+		assertNamespaceValues(result.value, expectedNsType, companyNsName);
+		verify(transaction).checkpoint(companyNsName,Env.ALWAYS);
+	}
+
+	private void assertNamespaceValues(Namespace value, NsType nsType, String namespaceName) {
+		List<String> people = Lists.newArrayList("tk007@people.osaaf.org");
+		assertEquals(Integer.valueOf(nsType.type), value.type);
+		assertEquals(namespaceName, value.name);
+		assertEquals("some namespace description", value.description);
+		assertEquals(people, value.admin);
+		assertEquals(people, value.owner);
+	}
+
+	private NsRequest createNsRequestForType(String nsName, String nsType) {
+		NsRequest req = new NsRequest();
+		req.setType(nsType);
+		req.setName(nsName);
+		req.setDescription("some namespace description");
+		req.getAdmin().add("tk007@people.osaaf.org");
+		req.getResponsible().add("tk007@people.osaaf.org");
+		return req;
+	}
 
 	@Test
 	public void test() {
@@ -156,7 +286,6 @@ public class JU_Mapper_2_0 {
 	public void testUsers(){
 		assertTrue(true);
 	}
-	
-		
+
 	
 }
