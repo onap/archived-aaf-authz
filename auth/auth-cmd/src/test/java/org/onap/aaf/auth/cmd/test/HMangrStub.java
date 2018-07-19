f48e477a28cb32ccfc006d292caa2ea46114ef9b
IanHowell <ian.howell@att.com>
2018-07-19 16:20:00 -0500

    Fix up several JUnits which were using real HTTP
    Issue-ID: AAF-401
    Change-Id: I4a9555222a6b3e822240ca95410a7a356d9ed521
    Signed-off-by: IanHowell <ian.howell@att.com>

---
+++ auth/auth-cmd/src/test/java/org/onap/aaf/auth/cmd/test/HMangrStub.java
@@ -0,0 +1,54 @@
+/*******************************************************************************
+ * ============LICENSE_START====================================================
+ * * org.onap.aaf
+ * * ===========================================================================
+ * * Copyright © 2017 AT&T Intellectual Property. All rights reserved.
+ * * ===========================================================================
+ * * Licensed under the Apache License, Version 2.0 (the "License");
+ * * you may not use this file except in compliance with the License.
+ * * You may obtain a copy of the License at
+ * * 
+ *  *      http://www.apache.org/licenses/LICENSE-2.0
+ * * 
+ *  * Unless required by applicable law or agreed to in writing, software
+ * * distributed under the License is distributed on an "AS IS" BASIS,
+ * * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * * See the License for the specific language governing permissions and
+ * * limitations under the License.
+ * * ============LICENSE_END====================================================
+ * *
+ * *
+ ******************************************************************************/
+package org.onap.aaf.auth.cmd.test;
+
+import java.net.HttpURLConnection;
+import java.net.URI;
+
+import org.onap.aaf.cadi.Access;
+import org.onap.aaf.cadi.Locator;
+import org.onap.aaf.cadi.LocatorException;
+import org.onap.aaf.cadi.SecuritySetter;
+import org.onap.aaf.cadi.client.Rcli;
+import org.onap.aaf.cadi.client.Retryable;
+import org.onap.aaf.cadi.http.HMangr;
+
+public class HMangrStub extends HMangr {
+	
+	private Rcli<HttpURLConnection> clientMock;
+
+	public HMangrStub(Access access, Locator<URI> loc, Rcli<HttpURLConnection> clientMock) throws LocatorException {
+		super(access, loc);
+		this.clientMock = clientMock;
+	}
+
+	@Override public<RET> RET same(SecuritySetter<HttpURLConnection> ss, Retryable<RET> retryable) {
+		try {
+			return retryable.code(clientMock);
+		} catch (Exception e) {
+		}
+		return null;
+	}
+	@Override public<RET> RET oneOf(SecuritySetter<HttpURLConnection> ss, Retryable<RET> retryable, boolean notify, String host) {
+		return null;
+	}
+}
--- auth/auth-cmd/src/test/java/org/onap/aaf/auth/cmd/test/perm/JU_Create.java
+++ auth/auth-cmd/src/test/java/org/onap/aaf/auth/cmd/test/perm/JU_Create.java
@@ -21,78 +21,89 @@
  ******************************************************************************/
 package org.onap.aaf.auth.cmd.test.perm;
 
-import org.junit.Assert;
-import org.junit.Before;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
-import static org.mockito.Mockito.mock;
+import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.when;
 
+import org.junit.Before;
+
+import org.onap.aaf.auth.cmd.test.HMangrStub;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.io.Writer;
 import java.net.HttpURLConnection;
 import java.net.URI;
 import java.net.URISyntaxException;
 
-import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.mockito.runners.MockitoJUnitRunner;
 import org.onap.aaf.auth.cmd.AAFcli;
-import org.onap.aaf.auth.cmd.perm.Create;
-import org.onap.aaf.auth.cmd.perm.Perm;
-import org.onap.aaf.auth.cmd.role.Role;
-import org.onap.aaf.auth.cmd.test.JU_AAFCli;
+import org.onap.aaf.auth.cmd.ns.Create;
+import org.onap.aaf.auth.cmd.ns.NS;
 import org.onap.aaf.auth.env.AuthzEnv;
 import org.onap.aaf.cadi.CadiException;
 import org.onap.aaf.cadi.Locator;
 import org.onap.aaf.cadi.LocatorException;
 import org.onap.aaf.cadi.PropAccess;
 import org.onap.aaf.cadi.SecuritySetter;
-import org.onap.aaf.cadi.Locator.Item;
-import org.onap.aaf.cadi.http.HMangr;
-import org.onap.aaf.cadi.http.HRcli;
+import org.onap.aaf.cadi.client.Future;
+import org.onap.aaf.cadi.client.Rcli;
 import org.onap.aaf.misc.env.APIException;
 
 @RunWith(MockitoJUnitRunner.class)
 public class JU_Create {
+
+	@Mock private SecuritySetter<HttpURLConnection> ssMock;
+	@Mock private Locator<URI> locMock;
+	@Mock private Writer wrtMock;
+	@Mock private Rcli<HttpURLConnection> clientMock;
+	@Mock private Future<Object> futureMock;
 		
 	private static Create create;
-	PropAccess prop;
-	AuthzEnv aEnv;
-	Writer wtr;
-	Locator<URI> loc;
-	HMangr hman;	
-	AAFcli aafcli;
+
+	private NS ns;
+	private PropAccess access;
+	private HMangrStub hman;	
+	private AuthzEnv aEnv;
+	private AAFcli aafcli;
 	
 	@Before
 	public void setUp () throws NoSuchFieldException, SecurityException, Exception, IllegalAccessException {
-		prop = new PropAccess();
-		aEnv = new AuthzEnv();
-		wtr = mock(Writer.class);
-		loc = mock(Locator.class);
-		SecuritySetter<HttpURLConnection> secSet = mock(SecuritySetter.class);
-		hman = new HMangr(aEnv, loc);	
-		aafcli = new AAFcli(prop, aEnv, wtr, hman, null, secSet);
-		Role role = new Role(aafcli);
-		Perm perm = new Perm(role);
-		create = new Create(perm);
+		MockitoAnnotations.initMocks(this);
 		
+		when(clientMock.create(any(), any(), any())).thenReturn(futureMock);
+		when(clientMock.delete(any(), any(), any())).thenReturn(futureMock);
+		when(clientMock.update(any(), any(), any())).thenReturn(futureMock);
+
+		hman = new HMangrStub(access, locMock, clientMock);
+		access = new PropAccess(new PrintStream(new ByteArrayOutputStream()), new String[0]);
+		aEnv = new AuthzEnv();
+		aafcli = new AAFcli(access, aEnv, wrtMock, hman, null, ssMock);
+		
+		ns = new NS(aafcli);
+
+		create = new Create(ns);
 	}
 	
 	@Test
-	public void testExec() throws APIException, LocatorException, CadiException, URISyntaxException {
-		Item value = mock(Item.class);
-		Locator.Item item = new Locator.Item() {
-		};
-		when(loc.best()).thenReturn(value);
-		URI uri = new URI("http://www.oracle.com/technetwork/java/index.html");
-		when(loc.get(value)).thenReturn(uri);
-		SecuritySetter<HttpURLConnection> secSet = mock(SecuritySetter.class);
-		HRcli hcli = new HRcli(hman, uri, item, secSet);
-		String[] strArr = {"grant","ungrant","setTo","grant","ungrant","setTo"};
-		create._exec(0, strArr);
+	public void testError() throws APIException, LocatorException, CadiException, URISyntaxException {
+		create._exec(0, new String[] {"grant","ungrant","setTo","grant","ungrant","setTo"});
+		create._exec(4, new String[] {"grant","ungrant","setTo","grant","ungrant","setTo"});
+	}
+	
+	@Test
+	public void testSuccess1() throws APIException, LocatorException, CadiException, URISyntaxException {
+		when(futureMock.code()).thenReturn(202);
+		create._exec(0, new String[] {"grant","ungrant","setTo","grant","ungrant","setTo"});
+	}
 
+	@Test
+	public void testSuccess2() throws APIException, LocatorException, CadiException, URISyntaxException {
+		when(futureMock.get(any(Integer.class))).thenReturn(true);
+		create._exec(0, new String[] {"grant","ungrant","setTo","grant","ungrant","setTo"});
 	}
 	
 	@Test
