65c40b3bffb78a1a77d82fc74bbf633a6d0d906f
Instrumental <jonathan.gathman@att.com>
2018-07-21 11:58:16 -0500

    Fix errors found no Container mode
    Issue-ID: AAF-318
    Change-Id: I90cca45dc91e191594707fdabb54a552d4d90127
    Signed-off-by: Instrumental <jonathan.gathman@att.com>

--- conf/CA/truststore.sh
+++ conf/CA/truststore.sh
@@ -1,2 +1,5 @@
+
 echo "FYI, by convention, truststore passwords are 'changeit', but you may add something more sophisticated"
-openssl pkcs12 -export -name AAF_Root_CA -in certs/ca.crt -nokeys -out truststore.p12
+# PCKS12 does not appear to be able to mark CAs as Trusted
+# openssl pkcs12 -export -name AAF_Root_CA -in certs/ca.crt -nokeys -out truststore.p12
+keytool -importcert -file certs/ca.crt -trustcacerts -alias AAF_ROOT_CA -keystore truststore.jks
