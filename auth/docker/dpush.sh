7a239e7ddb0662e95db9b7624a77c8cacc43122c
Instrumental <jonathan.gathman@att.com>
2018-08-14 15:13:02 -0500

    Add config agent core to dpush
    Issue-ID: AAF-433
    Change-Id: I0ed47de5c48edd6494673587f24881f40f4ed232
    Signed-off-by: Instrumental <jonathan.gathman@att.com>

--- auth/docker/dpush.sh
+++ auth/docker/dpush.sh
@@ -5,7 +5,7 @@
 . ./d.props
 
 if ["$1" == ""]; then
-  AAF_COMPONENTS=`ls ../aaf_*HOT/bin | grep -v '\.'`
+  AAF_COMPONENTS="config agent core `ls ../aaf_*HOT/bin | grep -v '\.'`"
 else
   AAF_COMPONENTS=$1
 fi
@@ -14,5 +14,4 @@ for AAF_COMPONENT in ${AAF_COMPONENTS}; do
         # docker push ${DOCKER_REPOSITORY}/${ORG}/${PROJECT}/aaf_${AAF_COMPONENT}:${OLD_VERSION}
         docker push ${DOCKER_REPOSITORY}/${ORG}/${PROJECT}/aaf_${AAF_COMPONENT}:${VERSION}
         # docker push ${DOCKER_REPOSITORY}/${ORG}/${PROJECT}/aaf_${AAF_COMPONENT}:${NEW_VERSION}
-
 done
