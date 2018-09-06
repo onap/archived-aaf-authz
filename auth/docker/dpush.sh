c63a9baf37d73da4b1a9ac36226f06d5f9d32180
Instrumental <jonathan.gathman@att.com>
2018-09-06 16:13:55 -0500

    Add additional Images 2
    Issue-ID: AAF-420
    Change-Id: I8d716cb95876391a6fb7323dae7ee414cb73f44a
    Signed-off-by: Instrumental <jonathan.gathman@att.com>

--- auth/docker/dpush.sh
+++ auth/docker/dpush.sh
@@ -4,11 +4,11 @@
 # Pull in Variables from d.props
 . ./d.props
 
-AAF_COMPONENTS=$(cat components) core agent config
+AAF_COMPONENTS="$(cat components) core agent config"
 
 for AAF_COMPONENT in ${AAF_COMPONENTS}; do
         # docker push ${DOCKER_REPOSITORY}/${ORG}/${PROJECT}/aaf_${AAF_COMPONENT}:${OLD_VERSION}
-        docker push ${DOCKER_REPOSITORY}/${ORG}/${PROJECT}/aaf_${AAF_COMPONENT}:${VERSION}
-        docker push ${DOCKER_REPOSITORY}/${ORG}/${PROJECT}/aaf_${AAF_COMPONENT}:latest
+        echo docker push ${DOCKER_REPOSITORY}/${ORG}/${PROJECT}/aaf_${AAF_COMPONENT}:${VERSION}
+        echo docker push ${DOCKER_REPOSITORY}/${ORG}/${PROJECT}/aaf_${AAF_COMPONENT}:latest
         # docker push ${DOCKER_REPOSITORY}/${ORG}/${PROJECT}/aaf_${AAF_COMPONENT}:${NEW_VERSION}
 done
