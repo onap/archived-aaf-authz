f526c472a95d08104e5f0be04eafa406378401df
Arundathi Patil <arundpil@in.ibm.com>
2018-09-19 16:53:18 +0530

    SonarFix-ListAdminResponsible,ListbyName,ListChild
    Fixed sonar code-smells/issues across these files
    ListAdminResponsible,
    ListByName,
    ListChildren
    
    Issue-ID: AAF-508
    Change-Id: I993d2946c8e17caae9ce6dd9600dc52d98a1206b
    Signed-off-by: Arundathi Patil <arundpil@in.ibm.com>

--- auth/auth-cmd/src/main/java/org/onap/aaf/auth/cmd/ns/ListByName.java
+++ auth/auth-cmd/src/main/java/org/onap/aaf/auth/cmd/ns/ListByName.java
@@ -52,8 +52,8 @@ public class ListByName extends Cmd {
     }
 
     @Override
-    public int _exec(int _idx, final String ... args) throws CadiException, APIException, LocatorException {
-            int idx = _idx;
+    public int _exec(int idxValue, final String ... args) throws CadiException, APIException, LocatorException {
+            int idx = idxValue;
         final String ns=args[idx++];
         return same(new Retryable<Integer>() {
             @Override
