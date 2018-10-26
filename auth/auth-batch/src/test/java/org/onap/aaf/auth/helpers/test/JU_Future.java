05ef6f4b159071f52b1d01b6239808dec8441f32
Sai Gandham <sg481n@att.com>
2018-10-26 09:58:18 -0500

    Add more junits to batch module
    fixed test failure
    
    Issue-ID: AAF-111
    Change-Id: I9161af0391183a0806104608703183ecac6bdf72
    Signed-off-by: Sai Gandham <sg481n@att.com>

--- auth/auth-batch/src/test/java/org/onap/aaf/auth/helpers/test/JU_Future.java
+++ auth/auth-batch/src/test/java/org/onap/aaf/auth/helpers/test/JU_Future.java
@@ -21,88 +21,156 @@
 
 package org.onap.aaf.auth.helpers.test;
 
-import static org.junit.Assert.*;
-import org.junit.After;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.onap.aaf.auth.helpers.Future;
-
-import static org.mockito.Mockito.*;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import java.nio.ByteBuffer;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.UUID;
 
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import org.onap.aaf.auth.dao.cass.FutureDAO;
+import org.onap.aaf.auth.env.AuthzTrans;
+import org.onap.aaf.auth.helpers.Creator;
+import org.onap.aaf.auth.helpers.Future;
+import org.onap.aaf.auth.helpers.creators.RowCreator;
+import org.onap.aaf.auth.layer.Result;
+import org.onap.aaf.misc.env.Env;
+import org.onap.aaf.misc.env.LogTarget;
+import org.onap.aaf.misc.env.TimeTaken;
+import org.onap.aaf.misc.env.Trans;
+
+import com.datastax.driver.core.ResultSet;
+import com.datastax.driver.core.Row;
+import com.datastax.driver.core.Session;
+import com.datastax.driver.core.SimpleStatement;
 
 public class JU_Future {
-    
-    Future future;
-    Date start;
-    Date expires;
-    ByteBuffer bBuff;
-    
-    @Before
-    public void setUp() {
-        UUID id = new UUID(0, 0);
-        start = new Date();
-        expires = new Date();
-        future = new Future(id, "Re-Validate Ownership for AAF Namespace '\'test\'test","target",start, expires, bBuff);
-    }
 
-    @Test
-    public void testId() {
-        Assert.assertTrue(future.id() instanceof UUID);
-    }
-    
-    @Test
-    public void testMemo() {
-        Assert.assertEquals("Re-Validate Ownership for AAF Namespace '\'test\'test", future.memo());
-    }
-    
-    @Test
-    public void testStart() {
-        Assert.assertTrue(future.start() instanceof Date);
-    }
-    
-    @Test
-    public void testExpires() {
-        Assert.assertTrue(future.expires() instanceof Date);
-    }
-    
-    @Test
-    public void testTarget() {
-        Assert.assertEquals("target",future.target());
-    }
-    
-    @Test
-    public void testExpunge() {
-        future.expunge();
-    }
-    
-    @Test
-    public void testCompareTo() {
-        future.compareTo(null);
-        future.compareTo(future);
-    }
-    
-    @Test
-    public void testResetLocalData() {
-        future.resetLocalData();
-    }
-    
-    @Test
-    public void testSizeForDeletion() {
-        Assert.assertEquals(0, future.sizeForDeletion());
-    }
-    
-    @Test
-    public void testPendingDelete() {
-        Assert.assertEquals(false, future.pendingDelete(future));
-    }
-    
+	Future future;
+	Date start;
+	Date expires;
+	ByteBuffer bBuff;
+
+	@Before
+	public void setUp() {
+		UUID id = new UUID(0, 0);
+		start = new Date();
+		expires = new Date();
+		future = new Future(id, "Re-Validate Ownership for AAF Namespace '\'test\'test", "target", start, expires,
+				bBuff);
+	}
+
+	@Test
+	public void testId() {
+		Assert.assertTrue(future.id() instanceof UUID);
+	}
+
+	@Test
+	public void testMemo() {
+		Assert.assertEquals("Re-Validate Ownership for AAF Namespace '\'test\'test", future.memo());
+	}
+
+	@Test
+	public void testStart() {
+		Assert.assertTrue(future.start() instanceof Date);
+	}
+
+	@Test
+	public void testExpires() {
+		Assert.assertTrue(future.expires() instanceof Date);
+	}
+
+	@Test
+	public void testTarget() {
+		Assert.assertEquals("target", future.target());
+	}
+
+	@Test
+	public void testExpunge() {
+		future.expunge();
+	}
+
+	@Test
+	public void testCompareTo() {
+		future.compareTo(null);
+		future.compareTo(future);
+	}
+
+	@Test
+	public void testResetLocalData() {
+		future.resetLocalData();
+	}
+
+	@Test
+	public void testSizeForDeletion() {
+		Assert.assertEquals(0, future.sizeForDeletion());
+	}
+
+	@Test
+	public void testPendingDelete() {
+		Assert.assertEquals(false, future.pendingDelete(future));
+	}
+
+	@Test
+	public void testLoad() {
+		Session session = mock(Session.class);
+		Trans trans = mock(Trans.class);
+		Creator<Future> creator = mock(Creator.class);
+		LogTarget target = mock(LogTarget.class);
+		TimeTaken tt = mock(TimeTaken.class);
+		ResultSet results = mock(ResultSet.class);
+		ArrayList<Row> rows = new ArrayList<Row>();
+		Row row = RowCreator.getRow();
+		rows.add(row);
+
+		when(results.all()).thenReturn(rows);
+		when(trans.info()).thenReturn(target);
+		when(trans.start("Load Futures", Env.REMOTE)).thenReturn(tt);
+		when(trans.start("Process Futures", Env.SUB)).thenReturn(tt);
+		when(session.execute(any(SimpleStatement.class))).thenReturn(results);
+		when(creator.create(row)).thenReturn(future);
+
+		Future.load(trans, session, creator);
+	}
+
+	@Test
+	public void testV2() {
+		Future.v2_0_17.create(RowCreator.getRow());
+		assertEquals(Future.v2_0_17.select(), "select id,memo,target,start,expires from authz.future");
+	}
+
+	@Test
+	public void testWithConstruct() {
+		Future.withConstruct.create(RowCreator.getRow());
+		assertEquals(Future.withConstruct.select(), "select id,memo,target,start,expires,construct from authz.future");
+	}
+
+	@Test
+	public void testDelayedDeleteWithDryRun() {
+		AuthzTrans trans = mock(AuthzTrans.class);
+		LogTarget target = mock(LogTarget.class);
+
+		when(trans.info()).thenReturn(target);
+
+		assertEquals(Result.ok().status, future.delayedDelete(trans, null, true, "text").status);
+	}
+
+	@Test
+	public void testDelayedDeleteNonDryRun() {
+		AuthzTrans trans = mock(AuthzTrans.class);
+		LogTarget target = mock(LogTarget.class);
+		FutureDAO fd = mock(FutureDAO.class);
+
+		when(trans.info()).thenReturn(target);
+		when(fd.delete(any(AuthzTrans.class), any(FutureDAO.Data.class), any(Boolean.class))).thenReturn(Result.ok());
+
+		assertEquals(Result.ok().status, future.delayedDelete(trans, fd, false, "text").status);
+	}
 
 }
