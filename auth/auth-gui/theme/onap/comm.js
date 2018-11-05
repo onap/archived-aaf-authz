7a1817bf3cf3c40c6c33f673ddc46c3f115cc3bc
Instrumental <jonathan.gathman@att.com>
2018-11-05 11:11:15 -0600

    Missing Licenses
    Issue-ID: AAF-607
    Change-Id: Ibd6c50104af69c4164d53aa0f8709520020ce7a5
    Signed-off-by: Instrumental <jonathan.gathman@att.com>

--- auth/auth-gui/theme/onap/comm.js
+++ auth/auth-gui/theme/onap/comm.js
@@ -1,4 +1,24 @@
-function http(meth, sURL, sInput, func) {
+/**
+ * ============LICENSE_START====================================================
+ * org.onap.aaf
+ * ===========================================================================
+ * Copyright (c) 2018 AT&T Intellectual Property. All rights reserved.
+ * ===========================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END====================================================
+ *
+ */
+ function http(meth, sURL, sInput, func) {
 	if (sInput != "") { 
 		var http;
 		if (window.XMLHttpRequest) {// code for IE7+, Firefox, Chrome, Opera, Safari
--- docs/sections/installation/AAF-Integration-Guide.rst
+++ docs/sections/installation/AAF-Integration-Guide.rst
@@ -2,10 +2,6 @@
 .. http://creativecommons.org/licenses/by/4.0
 .. Copyright © 2017 AT&T Intellectual Property. All rights reserved.
 
-.. This work is licensed under a Creative Commons Attribution 4.0 International License.
-.. http://creativecommons.org/licenses/by/4.0
-.. Copyright © 2017 AT&T Intellectual Property. All rights reserved.
-
 AAF Integration Guide
 ============================
 .. code:: bash
