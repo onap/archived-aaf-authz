dcd6197d08d4c9defea5e46b0140c7652f8b4238
Driptaroop Das <driptaroop.das@in.ibm.com>
2019-02-01 12:52:25 +0530

    Sonar Fixes - NotifyCredExpiring.java
    Sonar Fixes - NotifyCredExpiring.java
    
    Issue-ID: AAF-739
    Change-Id: I163c5f31a3ccc41b1bc8f3af8e36a5eecdc61461
    Signed-off-by: Driptaroop Das <driptaroop.das@in.ibm.com>

--- auth/auth-batch/src/main/java/org/onap/aaf/auth/batch/update/NotifyCredExpiring.java
+++ auth/auth-batch/src/main/java/org/onap/aaf/auth/batch/update/NotifyCredExpiring.java
@@ -55,18 +55,10 @@ import org.onap.aaf.misc.env.util.Chrono;
 public class NotifyCredExpiring extends Batch {
 
     private static final String UNKNOWN_ID = "unknown@deprecated.id";
-    private static final String AAF_INSTANTIATED_MECHID = "AAF INSTANTIATED MECHID";
-    private static final String EXPIRATION_DATE = "EXPIRATION DATE";
-    private static final String QUICK_LINK = "QUICK LINK TO UPDATE PAGE";
-    private static final String DASH_1 = "-----------------------";
-    private static final String DASH_2 = "---------------";
-    private static final String DASH_3 = "----------------------------------------------------";
-    private static final String LINE = "\n----------------------------------------------------------------";
     private Email email;
     private int maxEmails;
     private final PrintStream ps;
     private final AuthzTrans noAvg;
-    private String supportEmailAddr;
 	private CSV csv;
 	private CSVInfo csvInfo;
 
@@ -111,7 +103,6 @@ public class NotifyCredExpiring extends Batch {
         
         if(args().length<2) {
         	System.err.println("Need Email Template");
-        	//quit = true;
         }
         if(quit) {
         	System.exit(2);
@@ -140,154 +131,6 @@ public class NotifyCredExpiring extends Batch {
         List<LastCred> noOwner = new ArrayList<>();
         ownerCreds.put(UNKNOWN_ID,noOwner);
         int emailCount=0;
-
-//        // Get a list of ONLY the ones needing email by Owner
-//        for (Entry<String, List<Cred>> es : Cred.byNS.entrySet()) {
-//            for (Cred c : es.getValue()) {
-//                List<UserRole> ownerURList = UserRole.getByRole().get(es.getKey()+".owner");
-//                if (ownerURList!=null) {
-//                    for (UserRole ur:ownerURList) {
-//                        String owner = ur.user();
-//                        List<LastCred> llc = ownerCreds.get(owner);
-//                        if (llc==null) {
-//                            ownerCreds.put(owner, (llc=new ArrayList<>()));
-//                        }
-//                        llc.add(new LastCred(c,last));
-//                    }
-//                } else {
-//                    noOwner.add(new LastCred(c,last));
-//                }
-//            }
-//        }
-//        
-//        boolean bCritical,bNormal,bEarly;
-//        Message msg = new Message();
-//        Notification ownNotf;
-//        StringBuilder logMessage = new StringBuilder();
-//        for (Entry<String,List<LastCred>> es : ownerCreds.entrySet()) {
-//            String owner = es.getKey();
-//            boolean header = true;
-//            try {
-//                Organization org = OrganizationFactory.obtain(env, owner);
-//                Identity user = org.getIdentity(noAvg, owner);
-//                if (!UNKNOWN_ID.equals(owner) && user==null) {
-//                    ps.printf("Invalid Identity: %s\n", owner);
-//                } else {
-//                    logMessage.setLength(0);
-//                    if (maxEmails>emailCount) {
-//                        bCritical=bNormal=bEarly = false;
-//                        email.clear();
-//                        msg.clear();
-//                        email.addTo(user==null?supportEmailAddr:user.email());
-//
-//                        ownNotf = Notification.get(es.getKey(),TYPE.CN);
-//                        if (ownNotf==null) {
-//                            ownNotf = Notification.create(user==null?UNKNOWN_ID:user.fullID(), TYPE.CN);
-//                        }
-//                        last = ownNotf.last;
-//                        // Get Max ID size for formatting purposes
-//                        int length = AAF_INSTANTIATED_MECHID.length();
-//                        for (LastCred lc : es.getValue()) {
-//                            length = Math.max(length, lc.cred.id.length());
-//                        }
-//                        String id_exp_fmt = "\t%-"+length+"s  %15s  %s";
-//
-//                        Collections.sort(es.getValue(),LastCred.COMPARE);
-//                        for (LastCred lc : es.getValue()) {
-//                            if (lc.last.after(must) && lc.last.before(early) && 
-//                                (ownNotf.last==null || ownNotf.last.before(withinLastWeek))) {
-//                                if (!bEarly && header) {
-//                                    msg.line("\tThe following are friendly 2 month reminders, just in case you need to schedule your updates early.  "
-//                                            + "You will be reminded next month\n");
-//                                    msg.line(id_exp_fmt, AAF_INSTANTIATED_MECHID,EXPIRATION_DATE, QUICK_LINK);
-//                                    msg.line(id_exp_fmt, DASH_1, DASH_2, DASH_3);
-//                                    header = false;
-//                                }
-//                                bEarly = true;
-//                            } else if (lc.last.after(critical) && lc.last.before(must) && 
-//                                    (ownNotf.last==null || ownNotf.last.before(withinLastWeek))) {
-//                                if (!bNormal) {
-//                                    boolean last2wks = lc.last.before(within2Weeks);
-//                                    if (last2wks) {
-//                                        try {
-//                                            Identity supvsr = user.responsibleTo();
-//                                            email.addCC(supvsr.email());
-//                                        } catch (OrganizationException e) {
-//                                            trans.error().log(e, "Supervisor cannot be looked up");
-//                                        }
-//                                    }
-//                                    if (header) {
-//                                        msg.line("\tIt is now important for you to update Passwords all all configurations using them for the following.\n" +
-//                                                (last2wks?"\tNote: Your Supervisor is CCd\n":"\tNote: Your Supervisor will be notified if this is not being done before the last 2 weeks\n"));
-//                                        msg.line(id_exp_fmt, AAF_INSTANTIATED_MECHID,EXPIRATION_DATE, QUICK_LINK);
-//                                        msg.line(id_exp_fmt, DASH_1, DASH_2, DASH_3);
-//                                    }
-//                                    header = false;
-//                                }
-//                                bNormal=true;
-//                            } else if (lc.last.after(tooLate) && lc.last.before(critical)) { // Email Every Day, with Supervisor
-//                                if (!bCritical && header) {
-//                                    msg.line("\t!!! WARNING: These Credentials will expire in LESS THAN ONE WEEK !!!!\n" +
-//                                             "\tYour supervisor is added to this Email\n");
-//                                    msg.line(id_exp_fmt, AAF_INSTANTIATED_MECHID,EXPIRATION_DATE, QUICK_LINK);
-//                                    msg.line(id_exp_fmt, DASH_1, DASH_2, DASH_3);
-//                                    header = false;
-//                                }
-//                                bCritical = true;
-//                                try {
-//                                    if (user!=null) {
-//                                        Identity supvsr = user.responsibleTo();
-//                                        if (supvsr!=null) {
-//                                            email.addCC(supvsr.email());
-//                                            supvsr = supvsr.responsibleTo();
-//                                            if (supvsr!=null) {
-//                                                email.addCC(supvsr.email());
-//                                            }
-//                                        }
-//                                    }
-//                                } catch (OrganizationException e) {
-//                                    trans.error().log(e, "Supervisor cannot be looked up");
-//                                }
-//                            }
-//                            if (bEarly || bNormal || bCritical) {
-//                                if (logMessage.length()==0) {
-//                                    logMessage.append("NotifyCredExpiring");
-//                                }
-//                                logMessage.append("\n\t");
-//                                logMessage.append(lc.cred.id);
-//                                logMessage.append('\t');
-//                                logMessage.append(Chrono.dateOnlyStamp(lc.last));
-//                                msg.line(id_exp_fmt, lc.cred.id, Chrono.dateOnlyStamp(lc.last)+"     ",env.getProperty(GUI_URL)+"/creddetail?ns="+Question.domain2ns(lc.cred.id));
-//                            }
-//                        }
-//                        
-//                        if (bEarly || bNormal || bCritical) {
-//                            msg.line(LINE);
-//                            msg.line("Why are you receiving this Notification?\n");
-//                                msg.line("You are the listed owner of one or more AAF Namespaces. ASPR requires that those responsible for "
-//                                        + "applications and their access review them regularly for accuracy.  The AAF WIKI page for AT&T is https://wiki.web.att.com/display/aaf.  "
-//                                        + "You might like https://wiki.web.att.com/display/aaf/AAF+in+a+Nutshell.  More detailed info regarding questions of being a Namespace Owner is available at https://wiki.web.att.com/pages/viewpage.action?pageId=594741363\n");
-//                                msg.line("You may view the Namespaces you listed as Owner for in this AAF Env by viewing the following webpage:\n");
-//                                msg.line("   %s/ns\n\n",env.getProperty(GUI_URL));
-//                            email.msg(msg);
-//                            Result<Void> rv = email.exec(trans, org,"");
-//                            if (rv.isOK()) {
-//                                ++emailCount;
-//                                if (!isDryRun()) {
-//                                    ownNotf.update(noAvg, session, false);
-//                                    // SET LastNotification
-//                                }
-//                                email.log(ps,logMessage.toString());
-//                            } else {
-//                                trans.error().log(rv.errorString());
-//                            }
-//                        }
-//                    }
-//                }
-//            } catch (OrganizationException e) {
-//                trans.info().log(e);
-//            }
-//        }
         trans.info().printf("%d emails sent for %s", emailCount,batchEnv);
     }
     
