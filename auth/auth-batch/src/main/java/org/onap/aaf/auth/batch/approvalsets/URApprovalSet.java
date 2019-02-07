b7d326c9560d2bdbd9c7cad76dfcdc4e17b61462
Arundathi Patil <arundpil@in.ibm.com>
2019-02-07 20:19:46 +0530

    Sonar fix: URApprovalSet.java
    Fixed sonar issues/code-smells across this file
    
    Issue-ID: AAF-751
    Change-Id: Ic5ae5831535ff034625aa372fb7a70e6c7f6e73a
    Signed-off-by: Arundathi Patil <arundpil@in.ibm.com>

--- auth/auth-batch/src/main/java/org/onap/aaf/auth/batch/approvalsets/URApprovalSet.java
+++ auth/auth-batch/src/main/java/org/onap/aaf/auth/batch/approvalsets/URApprovalSet.java
@@ -3,6 +3,8 @@
  * org.onap.aaf
  * ===========================================================================
  * Copyright (c) 2018 AT&T Intellectual Property. All rights reserved.
+ *
+ * Modifications Copyright (C) 2019 IBM.
  * ===========================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -109,7 +111,7 @@ public class URApprovalSet extends ApprovalSet {
 		}
 	}
 
-	private ApprovalDAO.Data newApproval(Data urdd) throws CadiException {
+	private ApprovalDAO.Data newApproval(Data urdd) {
 		ApprovalDAO.Data add = new ApprovalDAO.Data();
 		add.id = Chrono.dateToUUID(System.currentTimeMillis());
 		add.ticket = fdd.id;
