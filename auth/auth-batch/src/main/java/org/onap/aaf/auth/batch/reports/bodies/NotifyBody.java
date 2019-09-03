e56c3490c81a73d3bf457ea4eb3bdaa4e7613c9f
Thugutla sailakshmi <tsaila10@in.ibm.com>
2019-09-03 20:06:39 +0530

    Remove the declaration of thrown exception 'org.onap.aaf.misc.env.APIException', as it cannot be thrown from method's body
    Remove the declaration of thrown exception
    
    Issue-ID: AAF-876
    Change-Id: I49cc32eab3298c445d4a9aca5ee3007badf26431
    Signed-off-by: Thugutla sailakshmi <tsaila10@in.ibm.com>

--- auth/auth-batch/src/main/java/org/onap/aaf/auth/batch/reports/bodies/NotifyBody.java
+++ auth/auth-batch/src/main/java/org/onap/aaf/auth/batch/reports/bodies/NotifyBody.java
@@ -3,6 +3,7 @@
  * org.onap.aaf
  * ===========================================================================
  * Copyright (c) 2018 AT&T Intellectual Property. All rights reserved.
+ * Modifications Copyright (C) 2019 IBM.
  * ===========================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -172,7 +173,7 @@ public abstract class NotifyBody {
      * @throws URISyntaxException 
      * 
      */
-    public static void load(Access access) throws APIException, IOException {
+    public static void load(Access access) throws IOException {
         // class load available NotifyBodies
         ClassLoader cl = Thread.currentThread().getContextClassLoader();
         Package pkg = NotifyBody.class.getPackage();
