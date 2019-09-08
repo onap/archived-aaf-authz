d4024d8100a363db4b849f4d9f3dbe68f013ff70
Thugutla sailakshmi <tsaila10@in.ibm.com>
2019-09-08 23:03:38 +0530

    Annotate the "Loader" interface with the @FunctionalInterface annotation
    Annotate the "Loader" interface with the @FunctionalInterface annotation
    
    Issue-ID: AAF-876
    Change-Id: I0b7f804a79198ccc45e26af839a0008b02b2f57b
    Signed-off-by: Thugutla sailakshmi <tsaila10@in.ibm.com>

--- auth/auth-batch/src/main/java/org/onap/aaf/auth/batch/approvalsets/Loader.java
+++ auth/auth-batch/src/main/java/org/onap/aaf/auth/batch/approvalsets/Loader.java
@@ -3,6 +3,7 @@
  * org.onap.aaf
  * ===========================================================================
  * Copyright (c) 2018 AT&T Intellectual Property. All rights reserved.
+ * Modifications Copyright (C) 2019 IBM.
  * ===========================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -22,6 +23,7 @@ package org.onap.aaf.auth.batch.approvalsets;
 
 import org.onap.aaf.cadi.CadiException;
 
+@FunctionalInterface
 public interface Loader<T> {
     public T load() throws CadiException;
 }
