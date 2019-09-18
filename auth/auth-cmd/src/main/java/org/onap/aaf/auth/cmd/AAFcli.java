63d17a4c0bb0cfe76b5d1f703a9ce3ec02a52674
Thugutla sailakshmi <tsaila10@in.ibm.com>
2019-09-18 19:30:40 +0530

    AAFcli.java -Extract the assignment out of this expression
    AAFcli.java -Extract the assignment out of this expression
    
    Issue-ID: AAF-876
    Change-Id: Idee610c4076e07619481302ee1489394521fb8ed
    Signed-off-by: Thugutla sailakshmi <tsaila10@in.ibm.com>

--- auth/auth-cmd/src/main/java/org/onap/aaf/auth/cmd/AAFcli.java
+++ auth/auth-cmd/src/main/java/org/onap/aaf/auth/cmd/AAFcli.java
@@ -4,7 +4,7 @@
  * ===========================================================================
  * Copyright (c) 2018 AT&T Intellectual Property. All rights reserved.
  *
- * Modifications Copyright (C) 2018 IBM.
+ * Modifications Copyright (C) 2019 IBM.
  * ===========================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -376,7 +376,8 @@ public class AAFcli {
         char last = 0;
         for (int i = 0; i < line.length(); ++i) {
             char ch;
-            if (Character.isWhitespace(ch = line.charAt(i))) {
+            ch = line.charAt(i);
+            if (Character.isWhitespace(ch)) {
                 if (start || last==',') {
                     continue; // trim
                 } else if (quote != 0) {
