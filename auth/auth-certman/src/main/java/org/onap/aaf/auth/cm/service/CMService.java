a1b6b5e39af9658b26842a0556fe9a1a98970273
Thugutla sailakshmi <tsaila10@in.ibm.com>
2019-09-24 23:23:33 +0530

    CMService.java-Merge this if statement with the enclosing one
    CMService.java-Merge this if statement with the enclosing one
    
    Issue-ID: AAF-876
    Change-Id: Ibcd4505969c41b389302b7895dcd9ea85648a1c5
    Signed-off-by: Thugutla sailakshmi <tsaila10@in.ibm.com>

--- auth/auth-certman/src/main/java/org/onap/aaf/auth/cm/service/CMService.java
+++ auth/auth-certman/src/main/java/org/onap/aaf/auth/cm/service/CMService.java
@@ -3,7 +3,7 @@
  * org.onap.aaf
  * ===========================================================================
  * Copyright (c) 2018 AT&T Intellectual Property. All rights reserved.
- * Modifications Copyright (C) 2018 IBM.
+ * Modifications Copyright (C) 2019 IBM.
  * ===========================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -277,15 +277,13 @@ public class CMService {
                 }
 
                 // Add Artifact listed FQDNs
-                if(!dynamic_sans) {
-	                if (add.sans != null) {
+                if((!dynamic_sans)&&(add.sans != null)) {
 	                    for (String s : add.sans) {
 	                        if (!fqdns.contains(s)) {
 	                            fqdns.add(s);
 	                        }
 	                    }
-	                }
-                }
+	            }
 
                 // Policy 2: If Config marked as Expired, do not create or renew
                 Date now = new Date();
