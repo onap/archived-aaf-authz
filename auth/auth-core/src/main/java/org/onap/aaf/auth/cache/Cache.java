a165db66624283c69d1ddb50c7a58577dd13075c
Thugutla sailakshmi <tsaila10@in.ibm.com>
2019-09-25 22:29:28 +0530

    Cache.java-Extract the assignment out of this expression
    Cache.java-Extract the assignment out of this expression
    
    Issue-ID: AAF-876
    Change-Id: I87be37bee18526e691ef841c0bd81754d5be2c3d
    Signed-off-by: Thugutla sailakshmi <tsaila10@in.ibm.com>

--- auth/auth-core/src/main/java/org/onap/aaf/auth/cache/Cache.java
+++ auth/auth-core/src/main/java/org/onap/aaf/auth/cache/Cache.java
@@ -3,6 +3,7 @@
  * org.onap.aaf
  * ===========================================================================
  * Copyright (c) 2018 AT&T Intellectual Property. All rights reserved.
+ * Modifications Copyright (C) 2019 IBM.
  * ===========================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -172,7 +173,8 @@ public class Cache<TRANS extends Trans, DATA> {
             cleanseTimer = new Timer("Cache Cleanup Timer");
             int cleanInterval = Integer.parseInt(env.getProperty(CACHE_CLEAN_INTERVAL,"60000")); // 1 minute clean cycles 
             int highCount = Integer.parseInt(env.getProperty(CACHE_HIGH_COUNT,"5000"));
-            cleanseTimer.schedule(clean = new Clean(env, cleanInterval, highCount), cleanInterval, cleanInterval);
+            clean = new Clean(env, cleanInterval, highCount);
+            cleanseTimer.schedule(clean, cleanInterval, cleanInterval);
         }
         
         for (String key : keys) {
