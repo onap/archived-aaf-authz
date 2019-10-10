92d1905ec71352ba8ba8bd9ebfb989dcf50830e8
Thugutla sailakshmi <tsaila10@in.ibm.com>
2019-10-10 11:09:50 +0530

    DAOException.java -This block of commented-out lines of code should be removed
    Issue-ID: AAF-876
    Change-Id: I77af743726b89aa057081d6ecbbf6caeac796de2
    Signed-off-by: Thugutla sailakshmi <tsaila10@in.ibm.com>

--- auth/auth-cass/src/main/java/org/onap/aaf/auth/dao/DAOException.java
+++ auth/auth-cass/src/main/java/org/onap/aaf/auth/dao/DAOException.java
@@ -3,6 +3,7 @@
  * org.onap.aaf
  * ===========================================================================
  * Copyright (c) 2018 AT&T Intellectual Property. All rights reserved.
+ * Modifications Copyright (C) 2019 IBM.
  * ===========================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -29,9 +30,6 @@ public class DAOException extends Exception {
     private static final long serialVersionUID = 1527904125585539823L;
 
 //    // TODO -   enum in result class == is our intended design, currently the DAO layer does not use Result<RV> so we still use these for now
-//    public final static DAOException RoleNotFoundDAOException = new DAOException("RoleNotFound");
-//    public final static DAOException PermissionNotFoundDAOException = new DAOException("PermissionNotFound");
-//    public final static DAOException UserNotFoundDAOException = new DAOException("UserNotFound");
 
     public DAOException() {
     }
