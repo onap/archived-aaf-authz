475da40b0d58acdf4dd1a3590c490ce7b95a1af8
Jonathan Gathman <jgonap@stl.gathman.org>
2020-04-25 14:28:13 -0500

    Add StandAlone Unix Configurations Include scripts, podman starts, minor code changes.
    Issue-ID: AAF-1137
    Signed-off-by: Jonathan Gathman <jgonap@stl.gathman.org>
    Change-Id: Ief72d93e607dbe62875f592978ee15084c8310d2

---
+++ auth/docker/podman_create.sh
@@ -0,0 +1,5 @@
+podman pod create --name "aaf.gathsys.com" --publish 9042,8100
+
+#--publish 8095:8095 --publish 8140:8140 --publish 8150:8150 --publish 8200:8200 --publish 8130:8130 --publish 9042:9042
+
+# --publish 80:8096 
